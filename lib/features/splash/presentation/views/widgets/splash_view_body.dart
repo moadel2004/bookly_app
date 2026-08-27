94a66444f7528289eda0c2d884d3ce67b7b129bc
moadel2004 <mostafaadel2988@gmail.com>
2026-08-27 18:42:41 +0300

    splash_view ui done

--- lib/features/splash/presentation/views/widgets/splash_view_body.dart
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -1,10 +1,24 @@
+import 'package:bookly_app/core/utils/assets.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class SplashViewBody extends StatelessWidget {
   const SplashViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Column();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SvgPicture.asset(
+          AssetsData.logo,
+          width: 250.w,
+          fit: BoxFit.contain,
+          //colorFilter: const ColorFilter.mode(Colors.white, BlendMode.srcIn),
+        ),
+      ],
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/features/splash/presentation/views/splash_view.dart';
 import 'package:flutter/material.dart';
 import 'package:get/route_manager.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart'; // 1. استدعاء المكتبة
 
 void main() {
   runApp(const BooklyApp());
@@ -11,6 +13,19 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(home: SplashView());
+    // 2. تغليف التطبيق بـ ScreenUtilInit
+    return ScreenUtilInit(
+      // دي الأبعاد الافتراضية اللي المصمم شغال بيها على Figma (تقدر تعدلها حسب مقاس تصميمك)
+      designSize: const Size(360, 690),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        return GetMaterialApp(
+          theme: ThemeData().copyWith(scaffoldBackgroundColor: kPrimaryColor),
+          debugShowCheckedModeBanner: false,
+          home: const SplashView(), // يفضل إضافة const هنا لتحسين الأداء
+        );
+      },
+    );
   }
 }
